ef8e92b129871a804a4aa357f65d62ba0c74aea4
陈勃宇 <terry.chen@cloudkeeper.cn>
2018-09-18 15:41:32 +0800

    引入路由

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import Header from './common/header/index'
 import store from './store/index';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
 class App extends Component {
   render() {
     return (
       <Provider store = { store }>
-        <Header></Header>
+        <div>
+          <Header></Header>
+          <BrowserRouter>
+            <div>
+              <Route path='/' exact render={() => <div>home</div>}></Route>
+              <Route path='/detail' exact render={() => <div>detail</div>}></Route>
+            </div>
+          </BrowserRouter>
+        </div>
+        
       </Provider>
     );
   }
