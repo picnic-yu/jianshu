26402e2defbceecfb80df85ab8c6e029f7cf179d
陈勃宇 <terry.chen@cloudkeeper.cn>
2018-09-18 16:43:46 +0800

    topic组件布局

--- src/pages/home/components/Topic.js
+++ src/pages/home/components/Topic.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import {TopicWrapper, TopicItem} from '../style';
 export default class Topic extends React.Component{
     render(){
         return(
-            <div>Topic</div>
+            <TopicWrapper>
+                <TopicItem>
+                    <img className='topic-pic' src="//upload.jianshu.io/collections/images/14/6249340_194140034135_2.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64" alt=""/>
+                    社会热点
+                </TopicItem>
+            </TopicWrapper>
         )
     }
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -22,3 +22,31 @@ export const HomeRight = styled.div`
     float:right;
 
 `;
+
+export const TopicWrapper = styled.div`
+    padding:20px 0 10px 0;
+    overflow:hidden;
+    margin-left:-10px;
+`;
+
+
+export const TopicItem = styled.div`
+    float:left;
+    background:#f7f7f7;
+    height:32px;
+    line-height:32px;
+    margin-left:18px;
+    padding-right:10px;
+    font-size:14px;
+    color:#000;
+    margin-bottom:18px;
+    border:1px solid #dcdcdc;
+    border-radius:4px;
+    .topic-pic{
+        width:32px;
+        height:32px;
+        display:block;
+        float:left;
+        margin-right:10px;
+    }
+`;
